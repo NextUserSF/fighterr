e7c5310b0d6dd2fcc065021961287f28e5b846ef
Peter A. Shevtsov <petr.shevtsov@gmail.com>
2013-11-14 19:58:35 +0400

    grunt-docco

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,12 +53,21 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+
+        docco: {
+            src: ['src/**/*.js'],
+            options: {
+                output: 'docs/'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-docco');
 
     grunt.registerTask('test', ['jshint', 'jasmine']);
     grunt.registerTask('default', ['test', 'uglify']);
